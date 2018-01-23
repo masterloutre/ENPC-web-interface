165e3ff39f0279d67f8049c1f5ae23dbb40ac798
JustineVuillemot <vuillemotjustine@gmail.com>
2018-01-23 18:15:26 +0100

    debut etudiant controller

--- php/Controllers/EtudiantController.php
+++ php/Controllers/EtudiantController.php
@@ -8,11 +8,59 @@ $array = [
   "nom" =>  "Rose",
   "prenom" => "Daphné",
   "promo" => "2020",
-  "num_etud" => "1231454444"
+  "num_etud" => "1231454444",
 ];
 
-$etudiant = new Etudiant($array);
+$etud_test = create_etudiant($array);
 
-echo $etudiant->get_nom();
+echo "id before : ".$etud_test->get_id()."<br>";
+
+add_etudiant($db, $etud_test);
+
+echo "id after : ".$etud_test->get_id();
+
+//create objet etudiant
+function create_etudiant($arrayEtudiant){
+    return $etudiant = new Etudiant($arrayEtudiant);
+}
+
+//ajout objet etudiant en bdd
+function add_etudiant($db, $etudiant){
+    try{
+        
+    $bdd_req = $db->prepare('INSERT INTO etudiant(nom, prenom, num_etud, promo, mdp) VALUES ("'.$etudiant->get_nom().'","'.$etudiant->get_prenom().'",'.$etudiant->get_num_etud().','.$etudiant->get_promo().',"test")');
+    $bdd_req->execute();
+        
+    }catch(PDOException $e){
+        echo "PDO ERROR : ".$e->getMessage();
+    }
+    
+    $etudiant->set_id($db->lastInsertId());
+}
+
+//delete where id = $id
+function delete_etudiant($id){
+    
+}
+
+//remplace tous les champs en bdd selon l'objet en paramètre
+function update_etudiant($etudiant){
+    
+}
+
+//retourne id where num_etud = $num_etud
+function etudiant_exists($num_etud){
+    
+}
+
+//get where id = $id
+function getEtudiant($id){
+    
+}
+
+//select *
+function getAll(){
+    
+}
 
  ?>
--- php/Global/connect.php
+++ php/Global/connect.php
@@ -5,7 +5,7 @@ $username = "root";
 $password = "";
 
 try {
-    $db = new PDO("mysql:host=$servername;dbname=enpc_game_bdd", $username, $password);
+    $db = new PDO("mysql:host=$servername;dbname=millenaire4;charset=utf8", $username, $password);
     // set the PDO error mode to exception
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully <br>";
