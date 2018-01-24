dc517a7c6abc158f0c6ff1aa5d3591328e728778
Damagae <daphnegm.rose@gmail.com>
2018-01-24 17:38:07 +0100

    Score methods

--- php/Controllers/ScoreController.php
+++ php/Controllers/ScoreController.php
@@ -9,36 +9,44 @@ function create_score($array_score)
   return new Score($array_score);
 }
 
-function add_score($db, Score $score)
+function add_score($db, Score $score, $etudiant_id, $enigme_id)
 {
-  if(score_exists($db, $score))
+  if(!score_exists($db, $score))
   {
     try {
-      $db_req = $db->prepare('INSERT INTO score
-        (nom)
-        VALUES ('.$score->get_nom().')');
+      $db_req = $db->prepare(
+        'INSERT INTO score
+         (points, tentatives, temps, aide, etudiant_id, enigme_id)
+         VALUES ('.$score->get_points().','.$score->get_tentatives().','.$score->get_temps().','.$score->get_aide().', '.$etudiant_id.', '.$enigme_id.')');
       $db_req->execute();
       $score->set_id($db->lastInsertId());
       return true;
     }
-    catch(PDOException $e) { echo "Insertion failed: " . $e->getMessage(); }
+    catch(PDOException $e) {
+      echo "Insertion failed: " . $e->getMessage();
+      return false;
+    }
   }
   else { return false; }
 }
 
 function update_score($db, Score $score)
 {
-  if(score_exists($db, $score))
+  if(score_exists($db, $score) && $score->get_id() != NULL)
   {
     try {
-      $db_req = $db->prepare('UPDATE score
-        SET nom = '.$score->get_nom().'
-        WHERE score.id = '.$enigme->get_id()
+      $db_req = $db->prepare(
+        'UPDATE score
+         SET points = "'.$score->get_points().'", tentatives = '.$score->get_tentatives().', temps = '.$score->get_temps().', aide = '.$score->get_aide().'
+         WHERE score.id = '.$score->get_id()
         );
       $db_req->execute();
+      return true;
     }
-    catch(PDOException $e) { echo "Update failed: " . $e->getMessage(); }
-    return true;
+    catch(PDOException $e) {
+      echo "Update failed: " . $e->getMessage();
+      return false;
+    }
   }
   else { return false; }
 }
@@ -48,9 +56,10 @@ function delete_score($db, Score $score)
   if(score_exists($db, $score))
   {
     try {
-      $db_req = $db->prepare('DELETE
-        FROM score
-        WHERE score.id = '.$id
+      $db_req = $db->prepare(
+        'DELETE
+         FROM score
+         WHERE score.id = '.$score->get_id()
         );
       $db_req->execute();
       return true;
@@ -60,19 +69,24 @@ function delete_score($db, Score $score)
   else { return false; }
 }
 
-function score_exists($db, $id)
+function score_exists($db, Score $score)
 {
-  try {
-    $db_req = $db->prepare('SELECT id
-      FROM score
-      WHERE score.id= '.$id
-      );
-    $db_req->execute();
-    $result = $db_req->fetchAll();
-  }
-  catch(PDOException $e) { echo "Selection failed: " . $e->getMessage(); }
+  if ($score->get_id() != NULL)
+  {
+    try {
+      $db_req = $db->prepare(
+        'SELECT id
+         FROM score
+         WHERE score.id= '.$score->get_id()
+        );
+      $db_req->execute();
+      $result = $db_req->fetchAll();
+    }
+    catch(PDOException $e) { echo "Selection failed: " . $e->getMessage(); }
 
-  if ($result != NULL) { return true; }
+    if ($result != NULL) { return true; }
+    else { return false; }
+  }
   else { return false; }
 }
 
@@ -86,17 +100,22 @@ function get_score($db, $id)
       );
     $db_req->execute();
     $result = $db_req->fetchAll();
-    return create_score($result[0]);
+    if (!empty($result))
+    {
+      return create_score($result[0]);
+    }
+    else { return false; }
   }
   catch(PDOException $e) { echo "Selection failed: " . $e->getMessage(); }
 }
 
-function get_all_score()
+function get_all_score($db)
 {
   try {
-    $db_req = $db->prepare('SELECT *
-      FROM score
-      ORDER BY id'
+    $db_req = $db->prepare(
+      'SELECT *
+       FROM score
+       ORDER BY id'
       );
     $db_req->execute();
     $score_tab = [];
@@ -105,9 +124,9 @@ function get_all_score()
     {
       for ($i = 0; $i < count($result); ++$i)
       {
-        $enigme_tab[] = create_score($result[$i]);
+        $score_tab[] = create_score($result[$i]);
       }
-      return $comptence_tab;
+      return $score_tab;
     }
     else { return false; }
   }
