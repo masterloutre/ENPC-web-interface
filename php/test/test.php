812f63bab7c8c50d60b5ead19bfd566df4a66199
Damagae <daphnegm.rose@gmail.com>
2018-01-25 12:14:47 +0100

    /test/ folder ignored in php

---
+++ php/test/test.php
@@ -0,0 +1,11 @@
+<?php
+
+include "../Global/connect.php";
+//include "./Global/global.php";
+require_once "../Controllers/ScoreController.php";
+require_once "../Controllers/EnigmeController.php";
+require_once "../Controllers/CompetenceController.php";
+require_once "../Controllers/SituationProController.php";
+
+
+?>
