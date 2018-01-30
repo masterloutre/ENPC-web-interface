166a6ca11c5562471ed4cbf28ae0fcee23695abb
JustineVuillemot <vuillemotjustine@gmail.com>
2018-01-30 20:01:24 +0100

    css situation pro barre dans enigme view

---
+++ php/Public/ratio_situ_pro.js
@@ -0,0 +1,14 @@
+var container = document.getElementsByClassName("situation_pro_bar");
+var color = ["#54bfc4", "#8c4e81", "#6c488b"];
+
+for(var i=0; i<container.length; i++){
+    var spanRatio = container[i].childNodes[1].childNodes;
+    for(var j=0; j<spanRatio.length; j++){
+        if(spanRatio[j].localName == "span"){
+            if(spanRatio[j].hasAttribute("data-size")){
+                spanRatio[j].style = "flex-grow : "+spanRatio[j].getAttribute("data-size")+"; height : 10px; background-color : "+color[j-1]+";";
+            }
+            
+        }
+    }
+}
--- php/Views/homeAdmin.php
+++ php/Views/homeAdmin.php
@@ -1,4 +1,7 @@
 <div class="wrapper">
+ <h3>Résultats</h3>
+  <a class="button" href="index.php?action=interface-enseignant-competence">Vue par compétence</a>
+  <a class="button" href="index.php?action=interface-enseignant-enigme">Vue par enigme</a>
  <h3>Gestion des données</h3>
   <a class="button" href="index.php?action=interface-admin&vue=liste&item=enigme">Accéder aux énigmes</a>
   <a class="button" href="index.php?action=interface-admin&vue=liste&item=etudiant">Accéder aux étudiants</a>
